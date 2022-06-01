790afd9e9ba6a3a9d5b6d591c672e1b40f6e6954
exceptr <98112445+exceptr@users.noreply.github.com>
2022-06-01 20:23:56 +0300

    get info and popular photos

--- vk.py
+++ vk.py
@@ -3,41 +3,84 @@ import vk_api
 from LiteVkApi import Client
 from vk_api.tools import VkTools
 from vk_api.execute import VkFunction
+from random import randrange
 
-token_group = "9408bb531621ecadc532e2756e8e7034130b7ace7f7551351bbeac71b0342226cd5c1e6dff04f7b543d14"
-session_group = vk_api.VkApi(token=token_group)
-vk_group = session_group.get_api()
 
-# vk_session = Client.give_session(session)
-# while True:
-#     if vk_session.check_new_msg():
-#         event = vk_session.get_event()
-#         eventxt, userid = event.text, event.user_id
-#         if eventxt == 'Привет':
-#             vk_session.msg(f'Привет, {userid}', userid)
-#         elif eventxt == 'Как дела?':
-#             vk_session.msg('Хорошо, а у тебя?', userid)
+def run_bot():
+    token_group = "9408bb531621ecadc532e2756e8e7034130b7ace7f7551351bbeac71b0342226cd5c1e6dff04f7b543d14"
+    session_group = vk_api.VkApi(token=token_group)
+    vk_group = session_group.get_api()
+    vk_session = Client.give_session(session_group)
+    while True:
+        if vk_session.check_new_msg():
+            event = vk_session.get_event()
+            eventxt, userid = event.text, event.user_id
+            if eventxt == 'Й':
+                people_info = get_people_by_parameters(18, 25, 1, "Москва")
+                first_name = people_info['first_name']
+                last_name = people_info['last_name']
+                link_people = people_info['link_people']
+                photos = people_info['photos']
+                vk_session.msg(f'{first_name} {last_name}\n{link_people}', userid)
+                for event_txt in photos:
+                    # vk_session.msg(f'Привет, {userid}', userid)
+                    vk_group.messages.send(attachment=event_txt, user_id=userid, random_id=randrange(10 ** 7))
+            elif eventxt == 'Как дела?':
+                vk_session.msg('Хорошо, а у тебя?', userid)
+
 
 token_user = "a67f00c673c3d4b12800dd0ba29579ec56d804f3c5f3bbcef5328d4b3981fa5987b951cf2c8d8b24b9abd"
 session_user = vk_api.VkApi(token=token_user)
 vk_user = session_user.get_api()
 
 
-def get_people_by_parameters(age_from=None, age_to=None, sex=None, hometown=str, status=6, has_photo=1, count=1):
+def get_people_by_parameters(age_from=None, age_to=None, sex=None, hometown=str, status=6, has_photo=1, count=1,
+                             fields='boolean'):
+    counter = increase_counter()
     response = vk_user.users.search(age_from=age_from, age_to=age_to, sex=sex, hometown=hometown, status=status,
-                                    has_photo=has_photo, count=count)
-    photos_dict = {}
+                                    has_photo=has_photo, offset=counter, count=count, fields=fields)
+    people_info = {}
     for i in response['items']:
-        people = response['items']
-        response2 = vk_user.photos.getAll(owner_id=i['id'], extended=1)
-        # pprint(response2['items'])
-        photos = response2['items']
-        for v in photos:
-            # pprint(v['likes']['count'])
-            # pprint(v['sizes'][-1])
-            photos_dict[v['sizes'][-1]['url']] = v['likes']['count']
-            # print(url_photo)
-    pprint(photos_dict)
+        pprint(i)
+        if i['is_closed'] == 'True':
+            increase_counter()
+        else:
+            people_info.update(id=i['id'])
+            people_info.update(first_name=i['first_name'])
+            people_info.update(last_name=i['last_name'])
+            people_info.update(link_people=f"https://vk.com/id{str(i['id'])}")
+            people_info.update(counter=counter)
+            pprint(people_info)
+
+    id = people_info['id']
+    response2 = vk_user.photos.getAll(owner_id=id, extended=1)
+    photos = response2['items']
+    photos_dict = {}
+    for v in photos:
+        attachment_photo = 'photo' + str(v['owner_id']) + '_' + str(v['id'])
+        photos_dict[attachment_photo] = v['likes']['count']
+        sorted_dict = sorted(photos_dict, key=photos_dict.__getitem__)
+        popular_photos = sorted_dict[-3:]
+        people_info.update(photos=popular_photos)
+    return people_info
 
 
-pprint(get_people_by_parameters(18, 25, 1, "Волжск"))
+counter_offset = {
+    'counter': 0
+}
+
+
+def increase_counter():
+    counter_offset['counter'] += 1
+    return counter_offset['counter']
+
+
+
+if __name__ == '__main__':
+    run_bot()
+    # get_people_by_parameters(18, 25, 1, "Москва")
+    # get_popular_photos()
+    # increase_counter()
+    # increase_counter()
+    # increase_counter()
+    # print(counter_offset)
