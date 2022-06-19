fb895f653f8e662fcaa149f853eb5103a74c5d4a
gwest-II <gwest0239@gmail.com>
2022-06-20 01:34:30 +0300

    Favorite two

---
+++ IntegretionDB/query_to_vkinder_db.py
@@ -0,0 +1,156 @@
+from Connect import connection
+from psycopg2 import Error
+
+
+# регистрация пользователя в БД
+def insert_data_userid(vk_id):
+    try:
+        cursor = connection.cursor()
+        insert_query = f"Insert into vkuser(vk_id) values ({vk_id});"
+        cursor.execute(insert_query, vk_id)
+        connection.commit()
+        count = cursor.rowcount
+        print('Запись добавлена', count)
+    except(Exception, Error) as error:
+        print('Возникла ошибка при добавлении:', error)
+
+
+# добавить в таблицу избранного
+def insert_data_favorite(favorite_link):
+    try:
+        cursor = connection.cursor()
+        insert_query = f"Insert into Favorite(favorite_link) values ('{favorite_link}');"
+        cursor.execute(insert_query, favorite_link)
+        connection.commit()
+        count = cursor.rowcount
+        print('Запись добавлена', count)
+    except(Exception, Error) as error:
+        print('Возникла ошибка при добавлении:', error)
+#
+# def insert_data_city(city):
+
+# добавить отношение юзера к избранному
+def insert_data_VKUserFavorite(id_user, id_favorite):
+    try:
+        cursor = connection.cursor()
+        insert_query = f"Insert into VKUserFavorite(id_user, id_favorite)" \
+                       f" values ({id_user}, {id_favorite});"
+        cursor.execute(insert_query, id_user)
+        connection.commit()
+        count = cursor.rowcount
+        print('Запись добавлена', count)
+    except(Exception, Error) as error:
+        print('Возникла ошибка при добавлении:', error)
+
+
+# добавить info о пользователе в БД
+def insert_query_userinfo(age, status, gender):
+    try:
+        cursor = connection.cursor()
+        query_userinfo = f"Insert into UserInfo(age, family_status, gender) values ({age}, {status}, {gender});"
+        cursor.execute(query_userinfo)
+        connection.commit()
+        count = cursor.rowcount
+        print('Запись добавлена', count)
+    except(Exception, Error) as error:
+        print('Возникла ошибка при добавлении:', error)
+
+
+# проверка id в базе
+def select_query_vkuser():
+    cursor = connection.cursor()
+    query_vkuser = "Select * from vkuser"
+    cursor.execute(query_vkuser)
+    vkuser_records = cursor.fetchall()
+    res = []
+    for i in vkuser_records:
+        res += [item for item in i]
+    return res
+# print(select_query_vkuser())
+
+
+def select_vkuser_id(vk_id):
+    cursor = connection.cursor()
+    query_vkuser = f"Select id from VKUser" \
+                   f" Where vk_id = {vk_id}"
+    cursor.execute(query_vkuser)
+    vkuser_records = cursor.fetchall()
+    res = []
+    for i in vkuser_records:
+        res += [item for item in i]
+        for id_ref in res:
+            return id_ref
+# print(select_vkuser_id('144344533'))
+
+
+def select_favorite_id():
+    cursor = connection.cursor()
+    query_vkuser = f"Select id from Favorite"
+    cursor.execute(query_vkuser)
+    vkuser_records = cursor.fetchall()
+    res = []
+    for i in vkuser_records:
+        res += [item for item in i]
+    return res[-1]
+# print(select_favorite_id())
+
+# показать список избранного
+def select_favorite(vk_id):
+    cursor = connection.cursor()
+    query_favorite = f"Select favorite_link From Favorite " \
+                     f"Join VKUserFavorite vf on Favorite.id = vf.id_favorite " \
+                     f"Join vkuser u on u.id = vf.id_user " \
+                     f"Where u.vk_id = {vk_id};"
+    cursor.execute(query_favorite)
+    favorite_records = cursor.fetchall()
+    new_list =[]
+    for link in favorite_records:
+        new_list += [item for item in link]
+        res = '\n'.join(new_list)
+    return res
+# print(select_favorite(144344533))
+
+# def select_query_city():
+#     cursor = connection.cursor()
+#     query_city = "Select * from city"
+#     cursor.execute(query_city)
+#     city_records = cursor.fetchall()
+#     return city_records[-1][0]
+
+
+# def insert_data_usercity():
+#     try:
+#         city_id = select_query_city()
+#         vk_id = select_query_vkuser()
+#         cursor = connection.cursor()
+#         insert_query = f"Insert into cityuser(id_city, id_user) values ({city_id}, {vk_id});"
+#         cursor.execute(insert_query, vk_id)
+#         connection.commit()
+#         count = cursor.rowcount
+#         print('Запись добавлена', count)
+#     except(Exception, Error) as error:
+#         print('Возникла ошибка при добавлении:', error)
+
+
+def stop_connect():
+    cursor = connection.cursor()
+    cursor.close()
+    connection.close()
+    print("Соединение с PostgresSQL закрыто")
+
+
+# def delete_data(city_id):
+#     try:
+#         cursor = connection.cursor()
+#         delete_query = f"Delete from vkuser where id = {city_id}"
+#         cursor.execute(delete_query, city_id)
+#         connection.commit()
+#         count = cursor.rowcount
+#         print(count, 'Удалено')
+#     except(Exception, Error) as error:
+#         print('Error', error)
+#     finally:
+#         if connection:
+#             cursor.close()
+#             connection.close()
+#             print("Соединение с PostgresSQL закрыто")
